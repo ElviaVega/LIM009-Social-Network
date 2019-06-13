c69a3b9f46f0dc4a33563202f1f883bfda32648d
ElviaVega <elvita100@hotmail.com>
2019-06-12 21:42:53 -0500

    primeros estilos

--- src/view/login.js
+++ src/view/login.js
@@ -1,21 +1,21 @@
 import { logInOnSubmit, logInGoogle } from "./view-controller.js";
 
 export default () => {
-    const formLogin  = document.createElement("div");
-    const tmpLogin = `
-        <div>
-        <form class=''>
-        <input type='email' id='email' placeholder ='Email' class='block'/>
-        <input type='password' id='password' placeholder ='Password' class='block'/>
-        <button type='button' id='btn-login' class='block'>Log in</button>
-        <p id='error'></p>
-        <p> O bien ingresa con: </p>
-        <a  id='btn-google'>
-			<img src='./imagen/logo-google.png' alt="Google" class='logo-google'>
-		</a>		
-        <p>Si no  tienes una cuenta <span><a href="#/register">Registrate</a></span></p>
-        </form>
-        `
+  const formLogin  = document.createElement("div");
+  const tmpLogin = `
+  <div>
+    <form class='form-login'>
+      <input type='email' id='email' placeholder ='Email' class='block'/>
+      <input type='password' id='password' placeholder ='Password' class='block'/>
+      <button type='button' id='btn-login' class='block'>Log in</button>
+      <p id='error'></p>
+      <p> O bien ingresa con: </p>
+      <a  id='btn-google'>
+        <img src='./imagen/logo-google.png' alt="Google" class='logo-google'>
+      </a>		
+      <p>Si no  tienes una cuenta <span><a href="#/register">Registrate</a></span></p>
+    </form>
+    `
     formLogin.innerHTML = tmpLogin;
    
     const btnlogIn = formLogin.querySelector('#btn-login');
--- src/view/welcome.js
+++ src/view/welcome.js
@@ -1,52 +1,46 @@
 import formPost from './post.js'
-//import showAllPosts from './showAllPosts.js'
 import {logOut} from './view-controller.js'
 import userProfile from './userProfile.js'
 import { getPost } from "../controler-firebase.js"
 import showPost from "./showPost.js"
-import post from './post.js';
+
 
 export default (user) => {
-    console.log(user)
-    const formWelcome  = document.createElement("div");
-    const tmpWelcome = `
-        <div >
-        <header>
-            <nav>
-                <ul class='menu-nav'>
-                    <li> perfil</li>
-                    <li> Consejos viajeros </li>
-                    <li><a id='sign-out'>Cerrar Sesión</a></li>             
-                 </ul>
-            </nav>
-        </header>
-        <h2> Bienvenido </h2>
-        
-        </div>       
-        `
-        formWelcome.innerHTML = tmpWelcome;
-        formWelcome.appendChild(formPost(user));
-        formWelcome.appendChild(userProfile(user));
-        const contenedorPost = document.createElement("div")
-        formWelcome.appendChild(contenedorPost)
-        getPost(posts => {               
-            contenedorPost.innerHTML = ""
-            console.log(posts)
-            //if(posts.visibility==="Todos"){
-                //console.log("bien hecho")}
-            posts.forEach((post)=> {
-                console.log(post)
-                const postElem = showPost(post,user)
-                if(post.visibility === "Todos"){ 
-                                   
-                    contenedorPost.appendChild(postElem)
-                }
-                else if (post.visibility === "Solo yo" && user.id === post.idUser){
-                        contenedorPost.appendChild(postElem)}
-            })
-            
-            
-        })
+//console.log(user)
+  const formWelcome  = document.createElement("div");
+  const tmpWelcome = `
+  <div >
+    <header>
+      <nav>
+        <ul class='menu-nav'>
+          <li> perfil</li>
+          <li> Consejos viajeros </li>
+          <li><a id='sign-out'>Cerrar Sesión</a></li>             
+        </ul>
+          </nav>
+    </header>
+      <h2> Bienvenido </h2>
+      <div class='imagen-portada'>
+        <img src="./imagen/colash.jpg">        
+      </div>       
+    `
+  formWelcome.innerHTML = tmpWelcome;
+  formWelcome.appendChild(formPost(user));
+  formWelcome.appendChild(userProfile(user));
+  const contenedorPost = document.createElement("div")
+  formWelcome.appendChild(contenedorPost)
+  getPost(posts => {               
+    contenedorPost.innerHTML = ""
+    //console.log(posts)
+    posts.forEach((post)=> {
+    //console.log(post)
+    const postElem = showPost(post,user)
+    if(post.visibility === "Todos"){                                    
+      contenedorPost.appendChild(postElem)
+    } else if(post.visibility === "Solo yo" && user.id === post.idUser){
+      contenedorPost.appendChild(postElem)}
+      })
+    })
              /*
         showAllPosts((postList) => {
             contenedorPost.innerHTML=""
@@ -54,11 +48,11 @@ export default (user) => {
         }, user);
         */
 
-        const btnSignOut = formWelcome.querySelector('#sign-out');
-        btnSignOut.addEventListener('click', logOut)
+    const btnSignOut = formWelcome.querySelector('#sign-out');
+    btnSignOut.addEventListener('click', logOut)
                
-        return formWelcome;
-        } 
+    return formWelcome;
+} 
         
 
  
